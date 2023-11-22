34526f42301acdebe00669e98d220e02af07ffe7
Kamilov Daniyarbek <Kamilov101205@gmail.com>
2023-11-22 22:52:21 +0300

    готово

---
+++ 3задание/3задание/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3задание
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Texta1.Clear();
+            if (!Double.TryParse(Texta.Text, out double K))
+            { MessageBox.Show("Поле только для чисел"); }
+            double D;
+            D = K % 7;
+            if (D == 7) { D = 0; }
+            Texta1.Text += ("День недели " + D);
+        }
+
+    }
+}
